61f2920541ca7d6e66b6cf21e6af63f1d57804c6
hicham sayidi <hicham13sayidi@gmail.com>
2024-06-15 15:15:04 +0100

    Prices route: ALLOW

--- routes/PricesRouter.js
+++ routes/PricesRouter.js
@@ -2,11 +2,10 @@ const express = require("express");
 const { getAllPrices,update } = require("../controllers/PriceController");
 
 const PriceRouter = express.Router();
-const { verifyTokenBackoffice} = require("../middlewares/authMiddlewares");
 
 
 // retrieve all prices 
-PriceRouter.get('/',verifyTokenBackoffice, getAllPrices);
+PriceRouter.get('/', getAllPrices);
 
 // update 
 
